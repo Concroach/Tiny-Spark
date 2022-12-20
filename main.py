5c4a06a8ca9a07632cbdfc958ca447f306dc9963
unknown <merkoncel@gmail.com>
2022-12-20 21:56:15 +0300

    20.12 notebook add a friend

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pygame, random, sys
 from pygame.locals import *
 import button
+from PIL import Image
 
 # Константы
 FPS = 60
-ENEMYSIZE = 10
+ENEMYSIZE = 15
+FRIENDSIZE = 20
 ENEMYMAXSIZE = 40
+FRIENDMAXSIZE = 25
 ENEMYMINSPEED = 1
 ENEMYMAXSPEED = 8
 ADDNEWENEMYRATE = 6
-PLAYERMOVERATE = 3
+ADDNEWFRIENDRATE = 30
+PLAYERMOVERATE = 6
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 700
@@ -21,24 +25,13 @@ LIGHTBLUE = (202, 228, 241)
 BLUE = (34, 145, 230)
 
 
-def enemyhit(playerRect, enemy):
-    for b in enemy:
-        if playerRect.colliderect(b['rect']):
-            return True
-    return False
-
-def drawText(text, font, surface, x, y, color):
-    textobj = font.render(text, 1, color)
-    textrect = textobj.get_rect()
-    textrect.topleft = (x, y)
-    surface.blit(textobj, textrect)
-
 pygame.init()
 Clock = pygame.time.Clock()
 font = pygame.font.SysFont(None, 48)
 pygame.display.set_caption("Tiny Spark")
 font_menu_name = pygame.font.SysFont(None, 96)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.mixer.music.load('baackground.mp3')
 
 # Загрузка изображений в меню
 start_img = pygame.image.load('images\menu\start_btn.png').convert_alpha()
@@ -47,9 +40,13 @@ exit_img = pygame.image.load('images\menu\exit_btn.png').convert_alpha()
 shop_img = pygame.image.load('images\menu\shop.png').convert_alpha()
 
 # Загрузка скинов
-player_image = pygame.image.load('images\items\creature-3.png')
+player_skin = 'images\items\minecraft_steve.jpg'
+player_image = pygame.image.load(player_skin)
 player_rect = player_image.get_rect()
-enemy_image = pygame.image.load('images\items\enemy.jpg')
+enemy_skin = 'images\items\minecraft_crepper.jpg'
+enemy_image = pygame.image.load(enemy_skin)
+friend_skin = 'images\items\enemy.jpg'
+friend_image = pygame.image.load(friend_skin)
 
 # Устанавливаем кнопки
 start_button = button.Button(420, 250, start_img, 1.6)
@@ -57,29 +54,69 @@ setting_button = button.Button(420, 340, setting_img, 1.6)
 exit_button = button.Button(420, 430, exit_img, 1.6)
 shop_button = button.Button(860, 560, shop_img, 0.8)
 
+# Музыка
+pygame.mixer.music.play()
+pygame.mixer.music.set_volume(5)
+
+# Соприкасание игрока с friend
+def friendhit(player_rect, friend):
+    for i in friend:
+        if player_rect.colliderect(i['rect']):
+            return True
+    return False
+
+# Соприкасание игрока с enemy
+def enemyhit(player_rect, enemy):
+    for i in enemy:
+        if player_rect.colliderect(i['rect']):
+            return True
+    return False
+
+# Функция для отрисовки текста
+def drawText(text, font, surface, x, y, color):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+
 running = True
 while running:
+    
     screen.fill(LIGHTBLUE)
     drawText('Tiny Spark', font_menu_name, screen, 346, 140, BLUE)
     
     if start_button.draw(screen):
         print('start the game')
+        game()
+    if setting_button.draw(screen):
+        print('settings')
+    if exit_button.draw(screen):
+        pygame.quit()
+        sys.exit()
+    if shop_button.draw(screen):
+        print('shop')
+    
+    # Функция гемплея
+    def game():
         pygame.mouse.set_visible(False)
         top_score = 0
         while True:
             enemy = []
+            friend = []
             score = 0
-            player_rect.topleft = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50)
+            im = Image.open(player_skin)
+            (player_width, player_height) = im.size
+            player_rect.topleft = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - player_height)
             moveLeft = False
             moveRight = False
             moveUp = False
             moveDown = False
             reverseCheat = False
             slowCheat = False
-            baddieAddCounter = 0
-
+            EnemyCounter = 0
+            FriendCounter = 0
             while True:
-                score += 1
                 for event in pygame.event.get():
                     if event.type == QUIT:
                         pygame.quit()
@@ -109,9 +146,9 @@ while running:
                             sys.exit()
                 # Спавн enemy
                 if True:
-                    baddieAddCounter += 1
-                if baddieAddCounter == ADDNEWENEMYRATE:
-                    baddieAddCounter = 0
+                    EnemyCounter += 1
+                if EnemyCounter == ADDNEWENEMYRATE:
+                    EnemyCounter = 0
                     enemyize = random.randint(ENEMYSIZE, ENEMYMAXSIZE)
                     new_enemy = {
                         'rect': pygame.Rect(random.randint(0, SCREEN_WIDTH-enemyize), 0 - enemyize, enemyize, enemyize),
@@ -119,6 +156,18 @@ while running:
                         'surface':pygame.transform.scale(enemy_image, (enemyize, enemyize)),
                         }
                     enemy.append(new_enemy)
+                # Спавн friend
+                if True:
+                    FriendCounter += 1
+                if FriendCounter == ADDNEWFRIENDRATE:
+                    FriendCounter = 0
+                    friend_size = random.randint(FRIENDSIZE, FRIENDMAXSIZE)
+                    new_friend = {
+                        'rect': pygame.Rect(random.randint(0, SCREEN_WIDTH-friend_size), 0 - friend_size, friend_size, friend_size),
+                        'speed': random.randint(ENEMYMINSPEED, ENEMYMAXSPEED),
+                        'surface':pygame.transform.scale(friend_image, (friend_size, friend_size)),
+                        }
+                    friend.append(new_friend)
 
                 # Персонаж не выходит за рамки и не поднимался выше половины высоты (позже стилизую так лвлa)
                 if moveLeft and player_rect.left > 0:
@@ -130,14 +179,21 @@ while running:
                 if moveDown and player_rect.bottom < SCREEN_HEIGHT:
                     player_rect.move_ip(0, PLAYERMOVERATE)
 
-                # Движение enemy
+                # Движение enemy и friend
                 for i in enemy:
                     i['rect'].move_ip(0, i['speed'])
 
-                # Удаляем enemy, вышедшие за экран
+                for i in friend:
+                    i['rect'].move_ip(0, i['speed'])
+
+                # Удаляем enemy и friend, вышедшие за экран
                 for i in enemy:
                     if i['rect'].top > SCREEN_HEIGHT:
                         enemy.remove(i)
+
+                for i in friend:
+                    if i['rect'].top > SCREEN_HEIGHT:
+                        friend.remove(i)
 
                 screen.fill(BLACK)
 
@@ -149,12 +205,23 @@ while running:
                 # Отрисовка игрока
                 screen.blit(player_image, player_rect)
 
-                # Отрисовка врагов
+                #.Отрисовка врагов
                 for i in enemy:
+                    screen.blit(i['surface'], i['rect'])
+                # Отрисовка друзей
+                for i in friend:
                     screen.blit(i['surface'], i['rect'])
 
                 pygame.display.update()
-
+                
+                # Проверка на соприкасание игрока и friend 
+                if friendhit(player_rect, friend):
+                    score += 1
+                    for i in friend:
+                        if player_rect.colliderect(i['rect']):                            
+                            friend.remove(i)
+                            
+                # Проверка на соприкасание игрока и enemy
                 if enemyhit(player_rect, enemy):
                     if score > top_score:
                         top_score = score
@@ -163,14 +230,6 @@ while running:
                 Clock.tick(FPS)
 
 
-    if setting_button.draw(screen):
-        print('settings')
-    if exit_button.draw(screen):
-        pygame.quit()
-        sys.exit()
-    if shop_button.draw(screen):
-        print('shop')
-    
     # Отрисовка кнопок через файл button.py
     start_button.draw(screen)
     setting_button.draw(screen)
